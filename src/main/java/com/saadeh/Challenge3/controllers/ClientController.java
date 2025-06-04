0c184669abb1f52fed53d499f13efc4cd733a3e8
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-06-04 08:42:42 -0300

    Creating CRUD for clients

--- src/main/java/com/saadeh/Challenge3/controllers/ClientController.java
+++ src/main/java/com/saadeh/Challenge3/controllers/ClientController.java
@@ -1,13 +1,17 @@
 package com.saadeh.Challenge3.controllers;
 
 import com.saadeh.Challenge3.dto.ClientDTO;
+import com.saadeh.Challenge3.entities.Client;
 import com.saadeh.Challenge3.services.ClientService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/clients")
@@ -21,4 +25,31 @@ public class ClientController {
         ClientDTO dto = service.findById(id);
         return ResponseEntity.ok(dto);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
+        Page page = service.findAll(pageable);
+        return ResponseEntity.ok(page);
+    }
+
+    @PostMapping
+    public ResponseEntity<ClientDTO> insert(@Valid @RequestBody ClientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> update(@PathVariable Long id, @Valid @RequestBody ClientDTO dto){
+        dto = service.update(id,dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/saadeh/Challenge3/services/ClientService.java
+++ src/main/java/com/saadeh/Challenge3/services/ClientService.java
@@ -3,8 +3,13 @@ package com.saadeh.Challenge3.services;
 import com.saadeh.Challenge3.dto.ClientDTO;
 import com.saadeh.Challenge3.entities.Client;
 import com.saadeh.Challenge3.repositories.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -17,5 +22,52 @@ public class ClientService {
     public ClientDTO findById(Long id){
         Client client = clientRepository.findById(id).orElseThrow(()-> new RuntimeException("Resource not found."));
         return new ClientDTO(client);
+    }
+
+    @Transactional
+    public Page<ClientDTO> findAll(Pageable pageable){
+        return clientRepository.findAll(pageable).map(
+                x-> new ClientDTO(x)
+        );
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client client = new Client();
+        copyDtoToEntity(dto,client);
+        client = clientRepository.save(client);
+        return new ClientDTO(client);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id, ClientDTO dto){
+        try {
+            Client client = clientRepository.getReferenceById(id);
+            copyDtoToEntity(dto,client);
+            client = clientRepository.save(client);
+            return new ClientDTO(client);
+        }catch (EntityNotFoundException e){
+            throw new RuntimeException("Recurso não encontrado.");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id){
+        if (!clientRepository.existsById(id)){
+            throw new RuntimeException("Recurso não encontrado.");
+        }
+        try {
+            clientRepository.deleteById(id);
+        }catch (DataIntegrityViolationException e){
+            throw new RuntimeException("Falha de integridade referencial.");
+        }
+    }
+
+    private void copyDtoToEntity(ClientDTO dto, Client client) {
+        client.setName(dto.getName());
+        client.setCpf(dto.getCpf());
+        client.setIncome(dto.getIncome());
+        client.setBirthDate(dto.getBirthDate());
+        client.setChildren(dto.getChildren());
     }
 }
